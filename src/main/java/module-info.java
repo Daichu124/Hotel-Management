17af2758a6be9999c7836c884ae0dce503c7c17d
Daichu124 <huudaichu4210124@gmail.com>
2026-05-15 19:36:11 +0700

    Upgrade: Chuyển đổi sang JavaFX Maven structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.hotel_management {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.hotel_management to javafx.fxml;
+    exports org.example.hotel_management;
+}
